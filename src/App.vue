065246096c9b43143cb2960d08a9daba7b8dcc71
zxc0328 <120797887@qq.com>
2016-11-12 19:29:38 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="$style.app">
+    <div :class="$style.text">
+      some text
+    </div>
+    <div :class="$style.text1">
+      some text1
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created () {
+    console.log(this.$style)
+  },
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="sass" module>
+
+.app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.red {
+  color: red;
+}
+.text{
+
+  composes: large from "sass!./common/color.scss";
+  composes: red from "sass!./common/color.scss";
+}
+
+.text1{
+  font-size:18px;
+  composes: red from "sass!./common/color.scss";
+}
+
+</style>
